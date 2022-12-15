d9be047bf35b98b73593cc1a06fc85c29e8cf16c
Gabriel José Gomes <85190212+Gabrieljoseg@users.noreply.github.com>
2022-12-15 19:21:57 -0300

    Alteração do app.js

--- RasaUI/src/App.js
+++ RasaUI/src/App.js
@@ -1,13 +1,31 @@
-import './App.css';
-import Basic from "./components/Basic"
+/* caso use algum tipo de link 
+pois o recat nao usa a tag <a> se usa essa 
+biblioteca, e se usa dessa forma: 
+(<Link to='./CadastroUsuario'>Crie uma agora</Link>)*/
+import {Link} from 'react-router-dom';
+import styles from './App.module.css';
 
-function App() {
+function chatBot() {
   return (
-    <div className="App">
-      <h1>Shreyas</h1>
-      <Basic />
+    <div className={styles.body}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h1 className={styles.titulo}>Chat Bot SIDI</h1>
+          <img className={styles.img} src="LOGOSIDI.jpeg"   width="70px" height="70px"/>          
+        </div>
+        <div className={styles.body}>
+          <p className={styles.message}>Hello</p>
+          <p className={styles.user_message}>Hi</p>
+        </div>
+        <div className={styles.fooder}>
+          <form className={styles.form}>
+            <input className={styles.input} type="text" name="" placeholder="Digite aqui"/>
+            <button className={styles.button}>Enviar</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default chatBot;
